2470bd7177fd7350b2e4d905bb874b12afa23f80
Kenneth Martens <kennethmartens@MacBook-13-Retina-2015.local>
2022-03-25 13:41:26 -0500

    Simple Kafka SPI that transfers messages to a Kafka topic

--- src/main/java/io/kennethmartens/keycloak/KeycloakEventListener.java
+++ src/main/java/io/kennethmartens/keycloak/KeycloakEventListener.java
@@ -1,19 +1,25 @@
 package io.kennethmartens.keycloak;
 
+import io.kennethmartens.messaging.Producer;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.admin.AdminEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class KeycloakEventListener implements EventListenerProvider {
 
+    final Logger logger = LoggerFactory.getLogger(KeycloakEventListener.class);
+
     @Override
     public void onEvent(Event event) {
-
+        logger.info("Keycloak event: {}", event);
+        Producer.publishEvent(event.getType().toString(), event);
     }
 
     @Override
     public void onEvent(AdminEvent adminEvent, boolean b) {
-
+        logger.info("Keycloak admin event: {}", adminEvent);
     }
 
     @Override
--- src/main/java/io/kennethmartens/keycloak/KeycloakEventListenerFactory.java
+++ src/main/java/io/kennethmartens/keycloak/KeycloakEventListenerFactory.java
@@ -5,31 +5,29 @@ import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventListenerProviderFactory;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class KeycloakEventListenerFactory implements EventListenerProviderFactory {
 
+    final Logger logger = LoggerFactory.getLogger(KeycloakEventListener.class);
+
     @Override
     public EventListenerProvider create(KeycloakSession keycloakSession) {
-        return null;
+        return new KeycloakEventListener();
     }
 
     @Override
-    public void init(Config.Scope scope) {
-
-    }
+    public void init(Config.Scope scope) {}
 
     @Override
-    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
-
-    }
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {}
 
     @Override
-    public void close() {
-
-    }
+    public void close() {}
 
     @Override
     public String getId() {
-        return "keycloak-event-listener";
+        return "kafka-event-listener";
     }
 }
